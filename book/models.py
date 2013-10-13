d21193b73084d23fa6cc86c1601948b56cb73dae
Jumpei Ogawa <phanective@gmail.com>
2013-10-13 17:59:08 +0900

    Added MoneyFlowItem model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class MoneyFlowItem(models.Model):
+	amount	= models.BigIntegerField()
+	date	= models.DateField()
+	category = models.IntegerField(max_length=1)
