f8c40216e79ca2056252b874345290c659ec5706
Jumpei Ogawa <phanective@gmail.com>
2013-11-17 16:31:52 +0900

    Add RecentRegistry model When Purchase is registered, it automatically registered to RecentRegistry.

--- book/models.py
+++ book/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from datetime import timedelta
+from datetime import datetime
 
-class MoneyFlowItem(models.Model):
+class Purchase(models.Model):
 	amount	= models.BigIntegerField()
 	date	= models.DateField()
 	category = models.IntegerField(max_length=1)
+
+	def save(self, *args, **kwargs):
+		super(Purchase, self).save(*args, **kwargs)
+		RecentRegistry(purchase=self).save()
+
+class RecentRegistry(models.Model):
+	purchase = models.ForeignKey("Purchase")
+	register_datetime = models.DateTimeField(auto_now=True)
+
+	def save(self, *args, **kwargs):
+		# Delete data from 3 days before
+		RecentRegistry.objects.filter(register_datetime__lte=datetime.now() - timedelta(days=3)).delete()
+		super(RecentRegistry, self).save(*args, **kwargs)
